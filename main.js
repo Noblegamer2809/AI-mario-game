f75868368ee5e06cfcf33f18982f5b77ce1d5b4d
Noblegamer2809 <85750071+Noblegamer2809@users.noreply.github.com>
2022-11-10 20:52:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+    canvas.parent('canvas');
+
+	instializeInSetup(mario);
+
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+
+	poseNet = ml5.poseNet(video, modelLoaded); 
+    poseNet.on('pose', gotResults);
+}
+
+function draw() {
+	game()
+}
+
+function modelLoaded()
+{
+	console.log("model is loaded");
+}
+
+function gotResults(results)
+{
+if(results.length > 0)
+{
+	console.log(results);
+	noseX = results[0].pose.nose.x;
+	noseY = results[0].pose.nose.y;
+}
+}
+
+
+
+
+
+
+
+
+
+
+
